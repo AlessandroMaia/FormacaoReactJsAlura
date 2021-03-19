67a8c3330604269cce3b94a4333a444c633508d7
AlessandroMaia <ale.maia.alm@gmail.com>
2021-03-19 08:49:51 -0300

    Continuacao da implementacao bytebank

--- 1 - JS OO/projeto-atual/index.js
+++ 1 - JS OO/projeto-atual/index.js
@@ -1,13 +1,16 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 10000000000);
 const cliente2 = new Cliente("Alice", 500000000450);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaCorrenteAlice = new ContaCorrente(cliente2, 1002);
-contaCorrenteRicardo.transferir(100, contaCorrenteAlice);
+const contaCorrenteAlice = new ContaCorrente(cliente2, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaCorrenteRicardo);
 console.log(contaCorrenteAlice); 
-console.log("Quantidade de contas: ", ContaCorrente.numeroDeContas); 
+console.log(contaPoupanca);
+console.log("Quantidade de contas correntes: ", ContaCorrente.numeroDeContas); 
