70ffaaf91fab3ad06ca65c1417f617feeccb3393
AlessandroMaia <ale.maia.alm@gmail.com>
2021-03-19 09:39:17 -0300

    Implementando estudos de polimorfismo ao projeto

--- 1 - JS OO/projeto-atual/index.js
+++ 1 - JS OO/projeto-atual/index.js
@@ -1,7 +1,9 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js"
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const cliente1 = new Cliente("Ricardo", 10000000000);
 const cliente2 = new Cliente("Alice", 500000000450);
@@ -14,3 +16,12 @@ console.log(contaCorrenteRicardo);
 console.log(contaCorrenteAlice); 
 console.log(contaPoupanca);
 console.log("Quantidade de contas correntes: ", ContaCorrente.numeroDeContas); 
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678910);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 12345678911);
+
+const estaLogado = SistemaAutenticacao.login(diretor, "123456");
+
+console.log(estaLogado);
+
