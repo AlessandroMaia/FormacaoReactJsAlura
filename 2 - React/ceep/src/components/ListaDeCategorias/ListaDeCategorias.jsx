a2ea602c21d50b042125e5d1f23c9262ba04d941
AlessandroMaia <ale.maia.alm@gmail.com>
2021-03-23 15:51:54 -0300

    ciclo de vida das objetos

--- 2 - React/ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ 2 - React/ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import "./estilo.css";
 class ListaDeCategorias extends Component {
 
+    constructor(){
+        super();
+        this.state = {categorias:[]};
+        this._novasCategorias = this._novasCategorias.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.categorias.inscrever(this._novasCategorias);
+    }
+
+    componentWillUnmount() {
+        this.props.categorias.desinscrever(this._novasCategorias);
+    }
+
+    _novasCategorias(categorias) {
+        this.setState({...this.state, categorias})
+    }
+
     _handleEventoInput(evento) {
         if (evento.key === "Enter") {
             let valorCategoria = evento.target.value;
@@ -14,7 +32,7 @@ class ListaDeCategorias extends Component {
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
                     {
-                        this.props.categorias.map((categoria, index) => {
+                        this.props.categorias.categorias.map((categoria, index) => {
                             return <li key={index} className="lista-categorias_lista-categorias">{categoria}</li>
                         }
                         )
