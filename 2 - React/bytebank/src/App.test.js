8025819907c2057662737bc29ac37e3e6bdf6f74
AlessandroMaia <ale.maia.alm@gmail.com>
2021-03-31 10:56:10 -0300

    Testando funções

--- 2 - React/bytebank/src/App.test.js
+++ 2 - React/bytebank/src/App.test.js
@@ -1,4 +1,4 @@
-import App from './app';
+import App, { calcularNovoSaldo } from './app';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
@@ -7,17 +7,39 @@ describe('Componente principal', () => {
         it('Nome é exibido', () => {
             render(<App />);
             expect(screen.getByText('ByteBank')).toBeInTheDocument();
-        })
+        });
 
         it('Saldo é exibido', () => {
             render(<App />);
             expect(screen.getByText('Saldo:')).toBeInTheDocument();
-        })
+        });
 
         it('Botão de transação é exibido', () => {
             render(<App />);
             expect(screen.getByText('Realizar operação')).toBeInTheDocument();
-        })
+        });
+    })
+
+    describe('Quando realizo uma transação', () => {
+        it('que é um saque, o valor vai diminuir', () => {
+            const valores = {
+                transacao: 'saque',
+                valor: 50
+            }
+            const novoSaldo = calcularNovoSaldo(valores, 150);
+            expect(novoSaldo).toBe(100);
+        });
+
+        it('que é um depósito, o valor vai aumentar', () => {
+            const valores = {
+                transacao: 'deposito',
+                valor: 50
+            }
+            const novoSaldo = calcularNovoSaldo(valores, 150);
+            expect(novoSaldo).toBe(200);
+        });
     })
 })
+
+
 
