0783c07fe75856591ccaaa33fa6f503e6f5830fb
AlessandroMaia <ale.maia.alm@gmail.com>
2021-04-01 10:05:49 -0300

    Testando componentes React

--- 2 - React/bytebank/src/App.test.js
+++ 2 - React/bytebank/src/App.test.js
@@ -1,6 +1,6 @@
 import App, { calcularNovoSaldo } from './app';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 describe('Componente principal', () => {
     describe('Quando eu abro o app do banco', () => {
@@ -37,6 +37,22 @@ describe('Componente principal', () => {
             }
             const novoSaldo = calcularNovoSaldo(valores, 150);
             expect(novoSaldo).toBe(200);
+        });
+
+        it('que é um saque, a transação deve ser realizada', () => {
+            render(<App />)
+
+            const saldo = screen.getByText('R$ 1000');
+            const transacao = screen.getByLabelText('Saque');
+            const valor = screen.getByTestId('valor');
+            const botaoTransacao = screen.getByText('Realizar operação');
+
+            expect(saldo.textContent).toBe('R$ 1000');
+            fireEvent.click(transacao, {target: {value: 'saque'}});
+            fireEvent.change(valor, { target: {value: 10}});
+            fireEvent.click(botaoTransacao);
+
+            expect(saldo.textContent).toBe('R$ 990');
         });
     })
 })
